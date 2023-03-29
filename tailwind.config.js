8c5a444c431b1fe85837829ad1f7ca36b65e99e2
Samuel Sunmonu <codebstack@gmail.com>
2023-03-29 06:06:12 +0100

    fix: file compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "1240px",
-      xl: "1640px",
+      sm: '480px',
+      md: '768px',
+      lg: '1240px',
+      xl: '1640px',
     },
 
     extend: {
       fontFamily: {
         // Mulish: ["Mulish", "sans-serif"],
-        WorkSans: ['Work Sans', "sans-serif"]
+        WorkSans: ['Work Sans', 'sans-serif'],
       },
       overflow: {
-        unset: "unset",
+        unset: 'unset',
       },
       colors: {
-        primary: "#39CDCC",
-        "primary-light": "#FFE5CD",
+        primary: '#39CDCC',
+        'primary-light': '#FFE5CD',
         primary_200: '#213F7D',
         primary_300: '#545F7D',
-        secondary: "#321E1F",
+        secondary: '#321E1F',
       },
       borderRadius: {
-        10: "10px",
-        16: "1rem",
-        20: "20px",
-        24: "24px",
-        25: "25px",
-        36: "36px",
-        128: "128px",
+        10: '10px',
+        16: '1rem',
+        20: '20px',
+        24: '24px',
+        25: '25px',
+        36: '36px',
+        128: '128px',
       },
       boxShadow: {
-        input: "0px 30px 30px rgba(131, 9, 0, 0.05)",
+        input:
+          '0px 30px 30px rgba(131, 9, 0, 0.05)',
       },
     },
   },
